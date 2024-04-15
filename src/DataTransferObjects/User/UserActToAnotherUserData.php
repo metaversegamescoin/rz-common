873fbed6ab59e6d3996fa1d454010ed3b565fce6
Mohsen Farzadipour <onemohsen@gmail.com>
2024-04-15 12:05:26 +0330

    chore: add user act to user dto

---
+++ src/DataTransferObjects/User/UserActToAnotherUserData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Metaversegamescoin\RzCommon\DataTransferObjects\User;
+
+class UserActToAnotherUserData
+{
+    public function __construct(
+        public readonly int $sourceUserId,
+        public readonly int $destinationUserId,
+    ) {}
+
+    /**
+     * @param array{sourceUserId: int,destinationUserId:int} $data
+     * @return UserData
+     */
+    public static function fromArray(array $data): self
+    {
+        return new static(
+            $data['sourceUserId'],
+            $data['destinationUserId'],
+        );
+    }
+}
