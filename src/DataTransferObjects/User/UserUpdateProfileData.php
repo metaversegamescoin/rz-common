b3377c0ff81772e847187885480ddacf0b79e4e4
MahdiBagheri71 <MahdiBagheriVar@gmail.com>
2024-05-25 12:45:47 +0330

    feat: add user update profile dto

---
+++ src/DataTransferObjects/User/UserUpdateProfileData.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Metaversegamescoin\RzCommon\DataTransferObjects\User;
+
+class UserUpdateProfileData
+{
+    public function __construct(
+        public readonly int $userId,
+        public readonly int $stateId,
+        public readonly int $countryId,
+        public readonly string $name,
+    ) {}
+
+
+}
