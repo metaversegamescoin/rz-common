2d8717b6d52befbbd856c9a3d77ab29f44e37755
MahdiBagheri71 <MahdiBagheriVar@gmail.com>
2024-07-14 16:12:08 +0330

    feat: add post data for social create post

---
+++ src/DataTransferObjects/Post/PostData.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Metaversegamescoin\RzCommon\DataTransferObjects\Post;
+
+class PostData
+{
+    public function __construct(
+        public readonly string $visibility,
+        public readonly ?string $message,
+        public readonly ?string $images,
+        public readonly ?bool $is_locked_comments,
+        public readonly ?bool $is_locked_post,
+        public readonly ?array $post_data,
+    ) {}
+
+
+    public static function fromArray(array $data): self
+    {
+        return new static(
+            $data['visibility'],
+            $data['message'] ?? null,
+                $data['images'] ?? null,
+            $data['is_locked_comments'] ?? false,
+            $data['is_locked_post'] ?? false,
+            $data['post_data'] ?? [],
+        );
+    }
+}
